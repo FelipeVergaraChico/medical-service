eb14f7d92ddfee76bc0efcf50cffabb068282d7a
Nicolas <nicolasuliana10@gmail.com>
2024-10-29 15:13:11 -0300

    Teste

--- src/components/form/OrsForm.jsx
+++ src/components/form/OrsForm.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import SignatureCanvas from "react-signature-canvas";
 import styles from "./OrsForm.module.css";
 import Input from "./Input";
